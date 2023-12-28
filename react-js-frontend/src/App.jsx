5facd75daf57b527c519428d9678a8ec2e13a4bd
Leandro_SmS <leandrosimoesms@gmail.com>
2023-12-28 10:27:27 -0300

    feat: Added toggle logic to toggle buttons

--- react-js-frontend/src/App.jsx
+++ react-js-frontend/src/App.jsx
@@ -1,10 +1,8 @@
-import CanteiroForm from './components/Canteiro-form'
-import Header from './components/Header'
-import ToggleBtn from './components/Toggle-btn'
+import CanteiroForm from './components/Canteiro-form';
+import Header from './components/Header';
+import ToggleBtn from './components/Toggle-btn';
 
-import headerImage from './assets/saf_bg.png'
-import btnImgExpandMore from './assets/expand_more.svg'
-import btnImgExpandLess from './assets/expand_less.svg'
+import headerImage from './assets/saf_bg.png';
 
 function App() {
 
@@ -21,8 +19,6 @@ function App() {
           <ToggleBtn
               btnId="addForm_toggleBtn"
               btnImgId="toggleBtnImgAdd"
-              btnImgSrc={btnImgExpandMore}
-              btnImgAlt="arrow-icon" 
               btnText="Adicionar Planta"
           />
         </section>
@@ -30,8 +26,6 @@ function App() {
           <ToggleBtn
               btnId="deleteForm_toggleBtn"
               btnImgId="toggleBtnImgDel"
-              btnImgSrc={btnImgExpandLess}
-              btnImgAlt="arrow-icon" 
               btnText="Deletar Planta"
           />
         </section>
--- react-js-frontend/src/components/Toggle-btn.jsx
+++ react-js-frontend/src/components/Toggle-btn.jsx
@@ -1,7 +1,39 @@
-function ToggleBtn({ btnId, btnImgId, btnImgSrc, btnImgAlt, btnText }) {
+import { useState } from 'react';
+
+import btnImgExpandMore from '../assets/expand_more.svg';
+import btnImgExpandLess from '../assets/expand_less.svg';
+
+function ToggleBtn({ btnId,
+                     btnImgId,
+                     btnText,
+                    }) {
+
+    // toggle buttons state
+    const [toggleForm, setToggleForm] = useState(false);
+
+    //handle toggle click btn
+    const handleToggleBtn = () => {
+      setToggleForm((t) => !t)
+    };
+
+    const imgInfo = {
+        src: toggleForm ? btnImgExpandLess : btnImgExpandMore,
+        alt: toggleForm ? "Expand less icon" : "Expand more icon"
+    }
+
     return (
-        <button type="button" className="toggleFormBtn" id={btnId}>
-            <img id={btnImgId} className="toggleBtnImg"  src={btnImgSrc} alt={btnImgAlt}/>
+        <button 
+            type="button" 
+            className="toggleFormBtn"
+            id={btnId}
+            onClick={handleToggleBtn}
+        >
+            <img 
+                id={btnImgId}
+                className="toggleBtnImg"
+                src={imgInfo.src}
+                alt={imgInfo.alt}
+            />
             {btnText}
         </button>
     )
