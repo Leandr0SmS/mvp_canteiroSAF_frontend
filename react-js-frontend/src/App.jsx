462b400ac017c4c027453bb6f7b8af2e3183d47a
Leandro_SmS <leandrosimoesms@gmail.com>
2024-02-17 07:26:09 -0300

    feat: Added TanStack Query q

--- react-js-frontend/src/App.jsx
+++ react-js-frontend/src/App.jsx
@@ -1,4 +1,8 @@
+import * as React from 'react'
 import { useEffect, useState } from 'react';
+
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import CanteiroForm from './components/Canteiro-form';
 import Header from './components/Header';
@@ -8,11 +12,32 @@ import DeleteForm from './components/Delete-form';
 
 import headerImage from './assets/saf_bg.png';
 
+// Create a query client
+const queryClient = new QueryClient()
+
+// Lazy load the devtools in production
+const ReactQueryDevtoolsProduction = React.lazy(() =>
+  import('@tanstack/react-query-devtools/build/modern/production.js').then(
+    (d) => ({
+      default: d.ReactQueryDevtools,
+    }),
+  ),
+)
+
 function App() {
+
+  // TanStack Query Devtool State
+  const [showDevtools, setShowDevtools] = React.useState(false)
 
   // Toggle Forms States
   const [addForm, setAddForm] = useState(false);
   const [delForm, setDelForm] = useState(false);
+
+  // Check Devtool
+  useEffect(() => {
+    // @ts-expect-error
+    window.toggleDevtools = () => setShowDevtools((old) => !old)
+  }, [])
 
   // Toggle button function
   const handleToggleBtn = (e) => {
@@ -30,6 +55,54 @@ function App() {
   }
 
   console.log([addForm, delForm])
+
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <>
+        <Header
+          imgSrc={headerImage}
+        />
+        <main id="main">
+          <CanteiroForm/>
+        </main>
+        <aside id="aside">
+          <section className="section--toggle">
+            <ToggleBtn
+                btnId="addForm_toggleBtn"
+                btnImgId="toggleBtnImgAdd"
+                btnText="Adicionar Planta"
+                toggleForm={addForm}
+                onClickToggleBtn={handleToggleBtn}
+            />
+            <AddForm
+              toggle={addForm}
+            />
+          </section>
+          <section className="section--toggle">
+            <ToggleBtn
+                btnId="deleteForm_toggleBtn"
+                btnImgId="toggleBtnImgDel"
+                btnText="Deletar Planta"
+                toggleForm={delForm}
+                onClickToggleBtn={handleToggleBtn}
+            />
+            <DeleteForm
+              toggle={delForm}
+            />
+          </section>
+        </aside>
+      </>
+      <ReactQueryDevtools initialIsOpen />
+      {
+        showDevtools && (
+          <React.Suspense fallback={null}>
+            <ReactQueryDevtoolsProduction />
+          </React.Suspense>
+        )
+      }
+    </QueryClientProvider>
+  )
 
   return (
     <>
